5f04e295b3bfb212ffc60fad9434a00b09bf6e5e
agmsbush <agmsbush@gmail.com>
2020-07-08 16:14:03 -0700

    Add GCB single app.

---
+++ gcb/single-app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import render_template
+import socket
+from datetime import datetime
+import logging
+import os
+import sys
+import requests
+import random
+
+app_version = "0.0.1"
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+datetime = datetime.now().replace(microsecond=0).isoformat()
+products = ['Google Kubernetes Engine'] + ['Anthos'] + ['Cloud Build']
+
+app = Flask(__name__)
+
+def get_product():
+    return(random.choice(products))
+
+product = get_product()
+
+@app.route("/")
+def hello():
+    return render_template('index.html',
+                            product=product,
+                            hostname=hostname,
+                            ip=ip,
+                            datetime=datetime,
+                            app_version=app_version)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False)
